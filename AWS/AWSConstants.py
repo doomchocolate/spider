b3b7e486b67f429a846b3fac20fc3dd7e1966c0a
Ubuntu <ubuntu@ip-172-31-2-182.ap-northeast-1.compute.internal>
2015-01-06 14:23:42 +0800

    change schema name

--- AWS/AWSConstants.py
+++ AWS/AWSConstants.py
@@ -1,4 +1,4 @@
 MYSQL_HOST     = "iam007.cskkndpfwwgp.ap-northeast-1.rds.amazonaws.com"
 MYSQL_PASSPORT = "jiangerji"
 MYSQL_PASSWORD = "eMBWzH5SIFJw5I4c"
-MYSQL_DATABASE = "iam007"
+MYSQL_DATABASE = "baidu"
