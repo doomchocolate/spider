3366f84667bbb16b99f9f9c51fda14a5a05eef51
jiangerji <jiangerji@gmail.com>
2015-01-28 20:53:59 +0800

    fix debug mode issue

--- JD/JDConstants.py
+++ JD/JDConstants.py
@@ -1,9 +1,11 @@
-DEBUG = True
+DEBUG = False
+if platform.system() == 'Windows':
+    DEBUG = True
 
 MYSQL_HOST     = "iam007.cskkndpfwwgp.ap-northeast-1.rds.amazonaws.com"
 MYSQL_PASSPORT = "jiangerji"
 MYSQL_PASSWORD = "eMBWzH5SIFJw5I4c"
-MYSQL_DATABASE = "spider"
+MYSQL_DATABASE = "baidu"
 
 if DEBUG:
     MYSQL_HOST="localhost"
