ee38b82a4a09027d9e7606bc608141d316cff51c
root <root@iZ25t7oiyw6Z.(none)>
2015-03-30 18:11:08 +0800

    update scheme version check update json

--- generate.sh
+++ generate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 python AppStore/AppStoreSpider.py generate
-mv AppStore/extSchemeApps.json.1 /alidata/www/web2py/applications/iam007/static/extSchemeApps.json
+cp AppStore/extSchemeApps.json.1 /alidata/www/web2py/applications/iam007/static/extSchemeApps.json
+cp -r AppStore/scheme /alidata/www/web2py/applications/iam007/static
